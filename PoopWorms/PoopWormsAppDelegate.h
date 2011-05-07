1c1854d327829e7307bd9bf6c229068724e84725
Mike Rotondo <mrotondo@gmail.com>
2011-05-07 12:54:36 -0700

    Initial Commit

---
+++ PoopWorms/PoopWormsAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  PoopWormsAppDelegate.h
+//  PoopWorms
+//
+//  Created by Mike Rotondo on 5/7/11.
+//  Copyright 2011 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class PoopWormsViewController;
+
+@interface PoopWormsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet PoopWormsViewController *viewController;
+
+@end
---
+++ PoopWorms/PoopWormsViewController.h
@@ -0,0 +1,15 @@
+//
+//  PoopWormsViewController.h
+//  PoopWorms
+//
+//  Created by Mike Rotondo on 5/7/11.
+//  Copyright 2011 Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PoopWormsViewController : UIViewController {
+    
+}
+
+@end
